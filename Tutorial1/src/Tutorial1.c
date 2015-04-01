813385d485de28f4adc38f4766c7132c057c7614
BrandonArroyo <barroyo50@gmail.com>
2015-03-31 22:31:10 -0500

    Testing the color

--- Tutorial1/src/Tutorial1.c
+++ Tutorial1/src/Tutorial1.c
@@ -5,7 +5,7 @@ static void main_window_load(Window *window) {
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorRed);
   text_layer_set_text(s_time_layer, "00:00");
 
   // Improve the layout to be more like a watchface
